2aa0897c6cdc549e4fde484c29f1959f49b21170
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2023-05-16 18:55:33 +0600

    Add files via upload

---
+++ 1822 C (CF).cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+
+
+    int t;
+    cin>>t;
+
+
+    while(t--){
+        long long int n;
+        cin>>n;
+
+        cout<<n*n+n*2+2<<endl;
+    }
+
+
+    return 0;
+
+}
