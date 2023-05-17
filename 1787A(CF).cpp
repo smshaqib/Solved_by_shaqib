8d55f7a259122eee8e9a4af6475673a8bb3518bc
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2023-05-17 17:24:05 +0600

    Add files via upload

---
+++ 1787A(CF).cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+
+    while(t--){
+
+
+        int n;
+        cin>>n;
+
+        if(n%2!=0){
+            cout<<"-1"<<endl;
+        }
+        else{
+            cout<<"1 "<<n/2<<endl;
+        }
+
+    }
+
+    return 0;
+}
