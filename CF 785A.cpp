3839ec13ef8e038ab958fed8321217bf4c2385bb
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2023-06-03 22:28:28 +0600

    Add files via upload

---
+++ CF 785A.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+int main(){
+
+
+    int t;
+    cin>>t;
+    int sum=0;
+
+    while(t--){
+
+        string s;
+        cin>>s;
+
+        if(s[0]=='T') sum+=4;
+
+        if(s[0]=='C') sum+=6;
+
+        if(s[0]=='O') sum+=8;
+
+        if(s[0]=='D') sum+=12;
+
+        if(s[0]=='I') sum+=20;
+    }
+
+    cout<<sum<<endl;
+
+    return 0;
+}
