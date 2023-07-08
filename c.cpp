c790743cb44a78c9c2595cab122df193eb4f93f0
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2023-07-08 18:02:15 +0600

    Add files via upload

---
+++ c.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define pi 3.1415926535897932384626433
+using namespace std;
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    double a,b,c;
+
+    cin>>a>>b>>c;
+
+    printf("MEDIA = %.1lf\n",(a*2.0+b*3.0+c*5.0)/10);
+
+}
