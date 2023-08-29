5a633b90065b1d3baa763c13210f47f2fd0933bf
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2023-08-29 19:02:53 +0600

    Add files via upload

---
+++ CF 1759.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+while t>=1:
+
+  s = 'YesYesYesYesYesYesYesYesYesYesYesYesYesYesYesYesYesYesYesYes'
+
+  sub= input()
+
+  if sub in s:
+    print("YES")
+  else:
+    print("NO")
+
+  t-=1
