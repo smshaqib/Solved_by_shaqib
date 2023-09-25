9c91d0afa14803607e8b7fb044b461f8490e4514
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2023-09-25 23:49:29 +0600

    Add files via upload

---
+++ CF 50A.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define VI vector<int>
+#define YES cout<<"YES\n";
+#define yes cout<<"yes\n";
+#define NO cout<<"NO\n";
+#define no cout<<"no\n";
+#define endl "\n";
+
+using namespace std;
+
+void solve( ){
+
+
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+//
+//    int t;
+//    cin>>t;
+//
+//    while(t--){
+//
+//        solve();
+//    }
+
+    int a,b;
+    cin>>a>>b;
+
+    int r = a*b;
+
+    r/=2;
+
+    cout<<r<<endl;
+}
+
+
