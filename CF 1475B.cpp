eaa3877601c7350a7e61ad8400e65244c802fbd0
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2023-10-02 11:59:09 +0600

    Add files via upload

---
+++ CF 1475B.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define VI vector<int>
+#define YES cout<<"YES\n";
+#define yes cout<<"yes\n";
+#define NO cout<<"NO\n";
+#define no cout<<"no\n";
+#define endl "\n";
+
+using namespace std;
+
+void solve( ){
+
+    int n;
+    cin>>n;
+
+    int x = n/2020;
+    int y = n%2020;
+
+    if(x>=y){
+        YES
+    }
+    else{
+        NO
+    }
+
+
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        solve();
+    }
+}
+
+
