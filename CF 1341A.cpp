9e96e79f4177f027a58db1248e2fed736b2df2e0
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2023-10-03 20:14:35 +0600

    Add files via upload

---
+++ CF 1341A.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define VI vector<int>
+#define YES cout<<"YES\n";
+#define yes cout<<"yes\n";
+#define NO cout<<"NO\n";
+#define no cout<<"no\n";
+#define endl "\n";
+
+using namespace std;
+
+void solve( ){
+
+    int n,a,b,c,d;
+
+    cin>>n>>a>>b>>c>>d;
+
+
+    int L = n*(a-b);
+    int R = n*(a+b);
+
+    if( R< c-d || L>c+d ){
+        cout<<"No"<<endl;
+    }
+
+    else{
+
+        cout<<"Yes"<<endl;
+    }
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        solve();
+    }
+}
+
+
