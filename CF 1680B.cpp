468c338119d6a501a4cbf7beca31ec896700c63b
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2023-10-27 00:44:52 +0600

    Add files via upload

---
+++ CF 1680B.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define VI vector<int>
+#define YES cout<<"YES\n";
+#define yes cout<<"yes\n";
+#define NO cout<<"NO\n";
+#define no cout<<"no\n";
+#define endl "\n";
+ 
+using namespace std;
+ 
+void solve( ){
+ 
+    int n,m;
+    cin>>n>>m;
+ 
+    char a[n+1][m+1];
+ 
+    int x=0,y=0,f=0;
+ 
+    for(int i=1; i<=n; i++){
+ 
+        for(int j=1; j<=m; j++){
+   
