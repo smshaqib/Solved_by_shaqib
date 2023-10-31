8685d3bbe464f89821534e47da3916f67884a160
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2023-10-31 11:41:25 +0600

    Add files via upload

---
+++ CF 1740A.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define VI vector<int>
+#define YES cout<<"YES\n";
+#define yes cout<<"yes\n";
+#define NO cout<<"NO\n";
+#define no cout<<"no\n";
+#define endl "\n";
+
+using namespace std;
+
+void solve( ){
+
+    int n;
+    cin>>n;
+
+    if(n==2){
+        cout<<2<<endl;
+        return;
+    }
+    cout<<3<<endl;
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        solve();
+    }
+}
+
+
