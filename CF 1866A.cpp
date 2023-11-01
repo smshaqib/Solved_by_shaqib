b350f32f751d13e39788f52068e5c219d2445b4e
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2023-11-01 18:46:04 +0600

    Add files via upload

--- CF 1866A.cpp
+++ CF 1866A.cpp
@@ -6,35 +6,41 @@
 #define NO cout<<"NO\n";
 #define no cout<<"no\n";
 #define endl "\n";
- 
+
 using namespace std;
- 
+
 void solve( ){
- 
-     int n;
-     cin>>n;
- 
-     int a[n-1];
-     int sum=0;
-     for(int i=0; i<n-1; i++){
+
+    int n;
+    cin>>n;
+
+    VI a(n);
+
+    int m=0;
+
+    for(int i=0; i<n; i++){
         cin>>a[i];
-        sum+=a[i];
-     }
- 
-     sum*=-1;
-     cout<<sum<<endl;
+
+        a[i]=abs(a[i]);
+    }
+
+    sort(a.begin(),a.end());
+
+    cout<<a[0]<<endl;
 }
- 
+
 int main(){
- 
+
     ios_base::sync_with_stdio(0);
     cin.tie(0);
- 
-    int t;
-    cin>>t;
- 
-    while(t--){
- 
+
+//    int t;
+//    cin>>t;
+//
+//    while(t--){
+
         solve();
-    }
+//    }
 }
+
+
