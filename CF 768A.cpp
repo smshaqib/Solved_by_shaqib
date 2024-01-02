99f6d3b5b2d55d06a2ccbd3530d9acbc4018177d
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2024-01-02 23:31:38 +0600

    Add files via upload

---
+++ CF 768A.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define VI vector<int>
+#define YES cout<<"YES\n";
+#define yes cout<<"yes\n";
+#define NO cout<<"NO\n";
+#define no cout<<"no\n";
+#define endl "\n";
+
+using namespace std;
+
+void solve( ){
+
+
+}
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+//
+//    int t;
+//    cin>>t;
+//
+//    while(t--){
+//
+//        solve();
+//    }
+
+    int n;
+    cin>>n;
+
+    VI a(n);
+
+    for(int i=0; i<n; i++)cin>>a[i];
+
+    sort(a.begin(),a.end());
+
+    int big = a[0];
+
+    int sml = a[n-1];
+
+    int c=0;
+
+    for(int i=0; i<n; i++){
+
+        if(a[i]!=big && a[i]!=sml){
+            c++;
+        }
+    }
+
+    cout<<c<<endl;
+}
+
