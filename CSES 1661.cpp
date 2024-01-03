573fd84f25cb7057b05402b9964555757c748fac
Sheikh Mohammad Shaqib <99066701+smshaqib@users.noreply.github.com>
2024-01-03 23:42:22 +0600

    Add files via upload

---
+++ CSES 1661.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define VI vector<int>
+#define YES cout<<"YES\n";
+#define yes cout<<"yes\n";
+#define NO cout<<"NO\n";
+#define no cout<<"no\n";
+#define endl "\n";
+
+using namespace std;
+
+void solve( ){
+
+
+}
+
+int const mx =  2e5+123;
+int a[mx];
+ll sum[mx];
+
+int main(){
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+//    int t;
+//    cin>>t;
+//
+//    while(t--){
+//
+//        solve();
+//    }
+
+    int n,x;
+    cin>>n>>x;
+
+    for(int i=1; i<=n; i++)cin>>a[i];
+
+    for(int i=1; i<=n; i++)sum[i]=sum[i-1]+a[i];
+
+
+    ll ans = 0;
+
+    map<ll,int>cnt;
+    cnt[0]=1;
+
+    for(int i=1; i<=n; i++){
+
+        ll SM = sum[i]-x;
+        ans+=cnt[SM];
+        cnt[sum[i]]++;
+    }
+
+    cout<<ans<<endl;
+}
+
